1aecdde6ff06ec5888bc479c9c6963a9b17a69ea
BrancoGarciaS <branco.garcia@usach.cl>
2022-10-09 23:14:35 -0300

    Desarrollo pixhex-d

--- Codigo_Fuente_21257371_GarciaSantana/main.pl
+++ Codigo_Fuente_21257371_GarciaSantana/main.pl
@@ -6,3 +6,10 @@ pixbit-d(X, Y, Bit, Profundidad, [X,Y,Bit,Profundidad]):-
     integer(Bit),
     Bit = 1; Bit = 0,
     integer(Profundidad).
+
+pixhex-d(X, Y, Color, Profundidad, [X,Y,Color,Profundidad]):-
+    integer(X),
+    integer(Y),
+    string(Color),
+    integer(Profundidad).
+
