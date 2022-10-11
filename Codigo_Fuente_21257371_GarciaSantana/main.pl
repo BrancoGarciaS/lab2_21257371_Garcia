414a962c172f4775a50ea1e368e66eded3bfb262
BrancoGarciaS <branco.garcia@usach.cl>
2022-10-10 23:53:34 -0300

    Desarrollo pixrgb-d

--- Codigo_Fuente_21257371_GarciaSantana/main.pl
+++ Codigo_Fuente_21257371_GarciaSantana/main.pl
@@ -1,4 +1,16 @@
-% Pixbit
+% X: entero que representa la coordenada X del pixel
+% Y: entero que representa la coordenada Y del pixel
+% Bit: color bit de un pixbit-d
+% Color: color string de un pixhex-d
+% Red: canal rojo de un pixrgb-d
+% Green: canal verde de un pixrgb-d
+% Blue: canal azul de un pixrgb-d
+% Profundidad: profundidad de un pixel
+/*
+ Predicados:
+ pixbit-d(X,Y,Bit,Profundidad, [X,Y,Bit,Profundidad])
+
+*/
 
 pixbit-d(X, Y, Bit, Profundidad, [X,Y,Bit,Profundidad]):-
     integer(X),
@@ -13,3 +25,16 @@ pixhex-d(X, Y, Color, Profundidad, [X,Y,Color,Profundidad]):-
     string(Color),
     integer(Profundidad).
 
+pixrgb-d(X, Y, Red, Green, Blue, Profundidad, [X,Y,Red,Green,Blue,Profundidad]):-
+    integer(X),
+    integer(Y),
+    integer(Red),
+    Red >= 0,
+    Red =< 255,
+    integer(Green),
+    Green >= 0,
+    Green =< 255,
+    integer(Blue),
+    Blue >= 0,
+    Blue =< 255,
+    integer(Profundidad).
